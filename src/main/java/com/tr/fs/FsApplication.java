2cc7537c7e7263698ace1b42458bb897080e3f70
unknown <dolarte@trustingrisk.com.co>
2023-12-17 12:23:00 -0500

    chore: initial project creation

---
+++ src/main/java/com/tr/fs/FsApplication.java
@@ -0,0 +1,14 @@
+package com.tr.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsApplication.class, args);
+		System.out.println("Hola Curso");
+	}
+
+}
