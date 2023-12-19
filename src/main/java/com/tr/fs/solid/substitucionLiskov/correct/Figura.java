29cfe5b286005ce3973de117c0ff056b7f091355
unknown <dolarte@trustingrisk.com.co>
2023-12-19 11:40:16 -0500

    section11: principios solid 3.Sustitucion de Liskov

---
+++ src/main/java/com/tr/fs/solid/substitucionLiskov/correct/Figura.java
@@ -0,0 +1,37 @@
+package com.tr.fs.solid.substitucionLiskov.correct;
+
+interface Figura {
+    int calcularArea();
+}
+
+class Rectangulo implements Figura {
+    protected int ancho;
+    protected int alto;
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+    @Override
+    public int calcularArea() {
+        return ancho * alto;
+    }
+}
+
+class Cuadrado implements Figura {
+    private int lado;
+
+    public void setSide(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public int calcularArea() {
+        return lado * lado;
+    }
+}
+
---
+++ src/main/java/com/tr/fs/solid/substitucionLiskov/incorrect/Rectangulo.java
@@ -0,0 +1,38 @@
+package com.tr.fs.solid.substitucionLiskov.incorrect;
+
+class Rectangulo {
+    protected int ancho;
+    protected int alto;
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+    public int calcularArea() {
+        return ancho * alto;
+    }
+}
+
+class Cuadrado extends Rectangulo {
+    @Override
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+        this.alto = ancho;
+    }
+
+    @Override
+    public void setAlto(int alto) {
+        this.alto = alto;
+        this.ancho = alto;
+    }
+}
+
+//Todo: En este escenario, la clase cuadrado hereda de Rectangulo.
+// Sin embargo, para asegurar que la propiedad del cuadrado en donde cada lado es igual,
+// Los setters en Cuadrado sobreescriben el comportamiento de la clase padre Rectangulo.
+// El sobreescribir el padre viola the Principio de sustitucion de liskov, pues cambia el
+// comportamiento esperado de la superclase.
