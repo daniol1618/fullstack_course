6e70418810358610b4aa948b01e9d51ecfebc304
unknown <dolarte@trustingrisk.com.co>
2023-12-25 12:14:24 -0500

    Seccion 16: Collections

--- src/main/java/com/tr/fs/lambdas/LambdasEstudio.java
+++ src/main/java/com/tr/fs/lambdas/LambdasEstudio.java
@@ -1,6 +1,13 @@
 package com.tr.fs.lambdas;
 
+import com.tr.fs.model.Genero;
+import com.tr.fs.model.Persona;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class LambdasEstudio {
 
@@ -23,9 +30,63 @@ public class LambdasEstudio {
         return calculadora.calcularTasaInteres(BigDecimal.TEN, BigDecimal.ONE);
     }
 
-    public static void main(String[] args) {
-        LambdasEstudio lambdasEstudio = new LambdasEstudio();
-        System.out.println(lambdasEstudio.calculosEstudio());
+
+    public void sortElements() {
+
+        ArrayList<String> nombresPersonas = new ArrayList<>(List.of("Gabriela", "Myriam", "Yaneth", "Fernando"));
+        ArrayList<Integer> enteros = new ArrayList<>(List.of(5, 4, 6, 2, 9, 44));
+
+        List<Persona> personas = new ArrayList<>(
+                List.of(
+                        new Persona("Daniel", 32, Genero.NOINFORMA),
+                        new Persona("Francis", 35, Genero.FEMENINO),
+                        new Persona("Caroline", 3, Genero.FEMENINO)
+                )
+        );
+
+        nombresPersonas.sort((a, b) -> {
+            return a.compareTo(b);
+        });
+
+        enteros.sort((a, b) -> {
+            return a.compareTo(b);
+        });
+
+
+        personas.sort((a, b) -> {
+            return a.getEdad().compareTo(b.getEdad());
+        });
+
+        System.out.println("Nombres Personas" + "\n" + nombresPersonas);
+        System.out.println("Enteros" + "\n" + enteros);
+        System.out.println("Personas:" + "\n" + personas);
+
+        //Ahora, utilizando el ordenamiento de la Clase Collections
+        ArrayList<String> nombresPersonas2 = new ArrayList<>(List.of("Gabriela", "Myriam", "Yaneth", "Fernando"));
+        ArrayList<Integer> enteros2 = new ArrayList<>(List.of(5, 4, 6, 2, 9, 44));
+
+        List<Persona> personas2 = new ArrayList<>(
+                List.of(
+                        new Persona("Daniel", 32, Genero.NOINFORMA),
+                        new Persona("Francis", 35, Genero.FEMENINO),
+                        new Persona("Caroline", 3, Genero.FEMENINO)
+                )
+        );
+
+        Collections.sort(nombresPersonas2, (a, b) -> {
+            return a.compareTo(b);
+        });
+
+        Collections.sort(enteros2);
+
+        Collections.sort(personas2, (a, b) -> {
+            return a.getNombre().compareTo(b.getNombre());
+        });
+
     }
 
+    public static void main(String[] args) {
+        LambdasEstudio lambdasEstudio = new LambdasEstudio();
+        lambdasEstudio.sortElements();
+    }
 }
